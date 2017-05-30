da910d8d4910620fc88d70f3b79fd68b05cd86c3
Santiago Melo <smeloa@yahoo.com>
2017-05-30 14:11:48 -0500

    ALmost finished work.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -175,62 +175,7 @@ $(document).ready(function () {
     }
     
     initCountTo();
-    
-    /*----------------------------------------
-    Ajax Contact Form
-    --------------------------------------*/
-	
-	// Function for email address validation
-	function isValidEmail(emailAddress) {
-        var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
 
-		return pattern.test(emailAddress);
-
-	}
-	$("#contactForm").on('submit', function (e) {
-		e.preventDefault();
-		var data = {
-			name: $("#name").val(),
-			email: $("#email").val(),
-            subject: $("#subject").val(),
-			message: $("#message").val()
-		};
-
-		if (isValidEmail(data['email']) && (data['message'].length > 1) && (data['name'].length > 1) && (data['subject'].length > 1)) {
-			$.ajax({
-				type: "POST",
-				url: "sendmail.php",
-				data: data,
-				success: function () {
-					$('#contactForm .input-success').delay(500).fadeIn(1000);
-					$('#contactForm .input-error').fadeOut(500);
-				}
-			});
-		} else {
-			$('#contactForm .input-error').delay(500).fadeIn(1000);
-			$('#contactForm .input-success').fadeOut(500);
-		}
-
-		return false;
-	});
-    
-    /*----------------------------------------
-    Newsletter Subscribe
-    --------------------------------------*/
-	$(".subscribe-form").ajaxChimp({
-		callback: mailchimpResponse,
-		url: "http://codepassenger.us10.list-manage.com/subscribe/post?u=6b2e008d85f125cf2eb2b40e9&id=6083876991" // Replace your mailchimp post url inside double quote "".  
-	});
-
-	function mailchimpResponse(resp) {
-        if (resp.result === 'success') {
-		 
-			$('.newsletter-success').html(resp.msg).fadeIn().delay(3000).fadeOut();
-			
-		} else if (resp.result === 'error') {
-			$('.newsletter-error').html(resp.msg).fadeIn().delay(3000).fadeOut();
-		}
-	}
     
     /*----------------------------------------
     Scroll to top
